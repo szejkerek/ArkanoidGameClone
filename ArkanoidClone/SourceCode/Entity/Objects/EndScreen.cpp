57cb2dd148d805f8ccf0a05ffd716dd92eef7fce
Bartek Gordon <bartekk.gordon@gmail.com>
2022-06-10 23:20:26 +0200

    endgame texture

--- ArkanoidClone/SourceCode/Entity/Objects/EndScreen.cpp
+++ ArkanoidClone/SourceCode/Entity/Objects/EndScreen.cpp
@@ -7,7 +7,7 @@ EndScreen::EndScreen(Program* _program): program(_program)
 	sf::Vector2f windowCenter = { static_cast<sf::Vector2f>(PixelSizes::GetInstance().windowResolution).x / 2,
 								  static_cast<sf::Vector2f>(PixelSizes::GetInstance().windowResolution).y / 2 };
 	background.setSize({ 800,300 });
-	background.setFillColor(sf::Color::Black);
+	background.setTexture(ResourceManager::Get().GetTexture("endgame"));
 	background.setOrigin({ background.getSize().x/2, background.getSize().y/2 });
 	background.setPosition(windowCenter);
 
@@ -17,6 +17,8 @@ EndScreen::EndScreen(Program* _program): program(_program)
 	text = new TextElement(_program);
 	text->SetTextPosition({ windowCenter.x, windowCenter.y - 75 });
 	text->SetText("GAME OVER!", 75, sf::Color::White);
+
+	
 
 }
 
